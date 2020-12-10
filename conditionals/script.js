8f39d067797213005263c1a954ef0a1cbfec2f0e
David Billups <davidtbillups@gmail.com>
2020-12-09 20:04:59 -0500

    Added a folder for conditionals and began working on conditionals practice problems

---
+++ conditionals/script.js
@@ -0,0 +1,27 @@
+/* Dec 9, 2020 - Foundations 3
+Conditionals Practice Problems*/
+
+// 1. Check Your Math
+// Use JavaScript to check and see if the following comparisons are true. For each of the following, log "True fact!" to the console if the condition evaluates to true and "LIES!" to the console if it doesn't.
+
+// Is the sum of 1 and 1 greater than 5?
+if (1+1 > 5) {
+    console.log("True fact!");
+} else {
+    console.log("LIES!");
+}
+// Is the product of 4 and 5 less than or equal to 20?
+if (4+5 <= 20) {
+    console.log("True fact!");
+} else {
+    console.log("LIES!");
+}
+// Is the difference between 6 and 2 greater than or equal to 0?
+if (6-2 >= 0) {
+    console.log("True fact!");
+} else {
+    console.log("LIES!");
+}
+
+// 2. Dog People vs. Cat People
+//
